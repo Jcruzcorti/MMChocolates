0615d9e8abd2a15e769ea5fc8fb8950e0fe50981
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-03-21 19:58:48 -0300

    Arreglos en js de contacto

--- app.js
+++ app.js
@@ -9,18 +9,32 @@ class Producto {
         this.nombre = nombre.toUpperCase();
         this.peso = peso.toLowerCase();
         this.producto = producto.toLowerCase();
+
+
+        if (this.peso=="1/4 kg", this.peso=="1/4kg", this.peso=="1/4"){
+            alert("Tu pedido tiene un valor de $700");
+        }
+        else if (this.peso=="1/2 kg", this.peso=="1/2kg", this.peso=="1/2"){
+            alert("Tu pedido tiene un valor de $1400");
+        }
+        else if (this.peso=="1 kg", this.peso=="1kg", this.peso=="1"){
+            alert("Tu pedido tiene un valor de $2800");
+        }
     }
+    
 
     pedido(){
-      return "Muchas gracias por elegirnos "+ this.nombre + ", tu pedido es " + this.peso + " de bombones " + this.producto + " de rellenos varios." + " Aprieta aceptar para confirmar, o actualiza la página para empezar de nuevo.";
+      return  "Muchas gracias por elegirnos "+ this.nombre + ", tu pedido es " + this.peso + " de bombones " + this.producto + " de rellenos varios." + " Aprieta aceptar para confirmar, o actualiza la página para empezar de nuevo.";
+
     }
 
 }
 
-let producto1 = new Producto (prompt("Ingresa tu nombre y apellido por favor"), prompt("Ingresa el peso en bombones que quieres comprar (1/4 kg, 1/2 kg, o 1 kg)"), prompt("Ingresa la variedad de bombones que quieres comprar (semi-amargos, dulces, y/o blancos)"));
+let producto1 = new Producto (prompt("Ingresa tu nombre y apellido por favor"), prompt("Ingresa el peso en bombones que quieres adquirir (1/4 kg, 1/2 kg, o 1 kg)"), prompt("Ingresa la variedad de bombones que quieres comprar (semi-amargos, dulces, y/o blancos)"));
 
 for (const productos in producto1) {
     console.log(producto1[productos]);
 }
 
 alert(producto1.pedido());
+
