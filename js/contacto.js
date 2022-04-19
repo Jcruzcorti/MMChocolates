b5764d7d47b7916f5614622e9db238515bb9234e
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-04-19 13:24:46 -0300

    Agregamos operadores avanzados para simplificar código

--- js/contacto.js
+++ js/contacto.js
@@ -11,33 +11,31 @@ let datos1 = document.getElementById ("input1")
 let datos2 = document.getElementById ("input2")
 let datos3 = document.getElementById ("input3")
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e)  => {
+
+    e.preventDefault();
+    
     console.log(datos1.value)
     console.log(datos2.value)
     console.log(datos3.value)
     
     console.log("El pedido del cliente es: " + input.value);
-    alert("cree este alert por que al apretar el boton enviar, se actualizaba instantáneamente la página y no dejaba ver el contenido")
 })
 
 
 
 // --------
 
-// A continuación, se verán los datos del primer pedido:
-localStorage.setItem("pedido",new Date()) 
 
-let primerPedido = localStorage.getItem("pedido")
+// Por si me olvido de consultarlo por la plataforma, quería dejarlo en algún lugar: tengo inconvenientes para poder agrander el tamaño del input de comentario (contancto.html). Antes era una etiqueta tipo text area, en vez de input, y estaba perfecto el tamaño del cuadrado, pero al cambiarlo por el input, se hizo pequeño (el tamaño actualmente). Entonces mi duda es no saber como hacer para incrementar el tamaño del input, gracias.
+
+
+
+// A continuación, se verán los datos del primer pedido: 
+
+
+let primerPedido = localStorage.getItem("pedido") || new Date()
+
+primerPedido ? console.log ("La fecha del pedido es: ", primerPedido) : localStorage.setItem("pedido",new Date()) 
 
 let jsonVariable = JSON.stringify(primerPedido)
-
-console.log(jsonVariable);
-
-
-
-
-
-
-
-
-
