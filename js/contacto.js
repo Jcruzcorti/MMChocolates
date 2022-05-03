6c98ddf2c40ff5b7c28ad31b3b0814d111a1731c
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-05-03 12:21:34 -0300

    Agregamos AJAX

--- js/contacto.js
+++ js/contacto.js
@@ -7,42 +7,40 @@ Swal.fire({
   confirmButtonText: "Aceptar",
 });
 
-const btn = document.getElementById ("botoncontacto1")
+// const btn = document.getElementById ("botoncontacto1")
 
 
-let input = document.getElementById ("entrada")
+// let input = document.getElementById ("entrada")
 
-let datos1 = document.getElementById ("input1")
-let datos2 = document.getElementById ("input2")
-let datos3 = document.getElementById ("input3")
+// let datos1 = document.getElementById ("input1")
+// let datos2 = document.getElementById ("input2")
+// let datos3 = document.getElementById ("input3")
 
-btn.addEventListener("click", (e)  => {
+// btn.addEventListener("click", (e)  => {
 
 
-    e.preventDefault();
+//     e.preventDefault();
     
-    console.log(datos1.value)
-    console.log(datos2.value)
-    console.log(datos3.value)
+//     console.log(datos1.value)
+//     console.log(datos2.value)
+//     console.log(datos3.value)
     
-    console.log("El pedido del cliente es: " + input.value);
+//     console.log("El pedido del cliente es: " + input.value);
 
 
-    Swal.fire({
-        position: "top-end", 
-        icon: "success",
-        title: "Su mensaje ha sido enviado",
-        showConfirmButton: false,
-        timer: 3000
-      })
+//     Swal.fire({
+//         position: "top-end", 
+//         icon: "success",
+//         title: "Su mensaje ha sido enviado",
+//         showConfirmButton: false,
+//         timer: 3000
+//       })
 
-})
+// })
 
 
 // --------
 
-
-// Por si me olvido de consultarlo por la plataforma, quería dejarlo en algún lugar: tengo inconvenientes para poder agrander el tamaño del input de comentario (contancto.html). Antes era una etiqueta tipo text area, en vez de input, y estaba perfecto el tamaño del cuadrado, pero al cambiarlo por el input, se hizo pequeño (el tamaño actualmente). Entonces mi duda es no saber como hacer para incrementar el tamaño del input, gracias.
 
 
 
@@ -59,3 +57,32 @@ let jsonVariable = JSON.stringify(primerPedido)
 
 // --------
 
+
+
+const btn = document.getElementById('botoncontacto1');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando mensaje...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_evfkc1n';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar mensaje';
+      Swal.fire({
+                position: "top-end", 
+                icon: "success",
+                title: "Su mensaje ha sido enviado",
+                showConfirmButton: false,
+                timer: 3000
+              })
+        
+    }, (err) => {
+      btn.value = 'Enviar mensaje';
+      alert(JSON.stringify(err));
+    });
+});
