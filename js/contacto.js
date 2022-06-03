a3b7808799620670159a86d2f22b7e0978ae6e22
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-06-03 13:19:09 -0300

    Ordenamos y creamos nuevas páginas dentro del sitio, incluyendo el carrito de compras

--- js/contacto.js
+++ js/contacto.js
@@ -2,60 +2,56 @@
 
 
 Swal.fire({
-  title: "MM Chocolates",
-  text: "A continuación si así lo deseas, podrás realizar tu pedido",
-  confirmButtonText: "Aceptar",
-});
-
-
-
-// --------
-
-
-let primerPedido = localStorage.getItem("pedido") || new Date()
-
-primerPedido ? console.log ("La fecha del pedido es: ", primerPedido) : localStorage.setItem("pedido",new Date()) 
-
-let jsonVariable = JSON.stringify(primerPedido)
-
-
-
-// --------
-
-
-
-const btn = document.getElementById('botoncontacto1');
-
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   btn.value = 'Enviando mensaje...';
-
-   const serviceID = 'default_service';
-   const templateID = 'template_evfkc1n';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Enviar mensaje';
-      Swal.fire({
-                position: "top-end", 
-                icon: "success",
-                title: "Su mensaje ha sido enviado",
-                showConfirmButton: false,
-                timer: 3000
-              })
+    title: "MM Chocolates",
+    text: "A continuación si así lo deseas, podrás realizar tu pedido",
+    confirmButtonText: "Aceptar",
+  });
+  
+  
+  
+  // --------
+  
+  
+  let primerPedido = localStorage.getItem("pedido") || new Date()
+  
+  primerPedido ? console.log ("La fecha del pedido es: ", primerPedido) : localStorage.setItem("pedido",new Date()) 
+  
+  let jsonVariable = JSON.stringify(primerPedido)
+  
+  
+  
+  // --------
+  
+  
+  
+  const btn = document.getElementById('botoncontacto1');
+  
+  document.getElementById('form')
+   .addEventListener('submit', function(event) {
+     event.preventDefault();
+  
+     btn.value = 'Enviando mensaje...';
+  
+     const serviceID = 'default_service';
+     const templateID = 'template_evfkc1n';
+  
+     emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Enviar mensaje';
+        Swal.fire({
+                  position: "top-end", 
+                  icon: "success",
+                  title: "Su mensaje ha sido enviado",
+                  showConfirmButton: false,
+                  timer: 3000
+                })
+            
+      }, (err) => {
+        btn.value = 'Enviar mensaje';
         
-    }, (err) => {
-      btn.value = 'Enviar mensaje';
-      
-      alert(JSON.stringify(err));
-    });
-});
+        alert(JSON.stringify(err));
+      });
 
+   
 
-
-
-
-
-
+    })
